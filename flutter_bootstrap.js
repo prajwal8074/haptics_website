73730422cb63edf320999c3d8bf8111feaa24c1d
Prajwal <prajwal8074@gmail.com>
2024-07-17 12:25:55 +0530

    final commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "449540765"
+    serviceWorkerVersion: "1148693544"
   }
 });
