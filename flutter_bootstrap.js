cd8c421a05f2d99eaa8bc1d818e7e956fc42a961
Prajwal <prajwal8074@gmail.com>
2024-07-19 05:09:18 +0530

    final

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2903113469"
+    serviceWorkerVersion: "4122564877"
   }
 });
